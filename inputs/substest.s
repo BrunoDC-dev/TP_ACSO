ce068098d59c2aa8e1456e74f9f6dd8751ab95bd
mariagonzalezdelsolar <gonzalezdelsolaem@udesa.edu.ar>
2025-03-29 14:58:26 -0300

    funciones aritmeticas

---
+++ inputs/substest.s
@@ -0,0 +1,5 @@
+.text
+mov X0, #5
+mov X1, #9
+subs X2, X0, X1
+hlt 0
--- src/arithmetic.c
+++ src/arithmetic.c
@@ -21,46 +21,113 @@ void adds_imm(uint32_t instruction) {
 void subs_imm(uint32_t instruction) {
     printf("subs_imm function enter\n");
     DecodedInstr d = decode_instruction(instruction, 1);
+
     if (d.shift){
       printf("shifting imm12\n");
       d.imm = d.imm << 12;
     }
+
     NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] - d.imm;
     update_flags(NEXT_STATE.REGS[d.rd]);
 }
 
+
+// void adds_reg(uint32_t instruction) {
+//     printf("adds_reg function enter\n");
+//     DecodedInstr d = decode_instruction(instruction, 0);
+    
+//     NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] + CURRENT_STATE.REGS[d.rm];
+//     update_flags(NEXT_STATE.REGS[d.rd]);
+// }
+
 void adds_reg(uint32_t instruction) {
-    printf("adds_reg function enter\n");
-    DecodedInstr d = decode_instruction(instruction, 0);
-    NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] + CURRENT_STATE.REGS[d.rm];
-    update_flags(NEXT_STATE.REGS[d.rd]);
+    DecodedInstr d = decode_instruction(instruction, 0); // R-type
+    uint32_t shift_type = get_bits(instruction, 21, 22); 
+    uint32_t shift_amount = get_bits(instruction, 10, 15); 
+
+    uint32_t operand1 = CURRENT_STATE.REGS[d.rn];
+    uint32_t operand2 = CURRENT_STATE.REGS[d.rm];
+
+    switch (shift_type) {
+        case 0: // LSL (Logical Shift Left)
+            operand2 = operand2 << shift_amount;
+            break;
+        case 1: // LSR (Logical Shift Right)
+            operand2 = operand2 >> shift_amount;
+            break;
+        case 2: // ASR (Arithmetic Shift Right)
+            operand2 = (int32_t)operand2 >> shift_amount;
+            break;
+        case 3: // ROR (Rotate Right)
+            operand2 = (operand2 >> shift_amount) | (operand2 << (32 - shift_amount));
+            break;
+        default:
+            break;
+    }
+
+    uint32_t result = operand1 + operand2;
+    NEXT_STATE.REGS[d.rd] = result;
+
+    update_flags(result);
 }
   
-void subs_reg(uint32_t instruction) {
-    printf("subs_reg function enter\n");
-    DecodedInstr d = decode_instruction(instruction, 0);
+// void subs_reg(uint32_t instruction) {
+//     printf("subs_reg function enter\n");
+//     DecodedInstr d = decode_instruction(instruction, 0);
     
-    if (d.rd == 31) {
-        // CMP - solo actualiza flags
-        printf("cmp_reg function enter\n");
-        int64_t result = CURRENT_STATE.REGS[d.rn] - CURRENT_STATE.REGS[d.rm];
-        update_flags(result);
-    } else {
-        // SUBS - actualiza flags y guarda resultado
-        printf("subs_reg function enter\n");
-        NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] - CURRENT_STATE.REGS[d.rm];
-        update_flags(NEXT_STATE.REGS[d.rd]);
+//     if (d.rd == 31) {
+//         // CMP - solo actualiza flags
+//         printf("cmp_reg function enter\n");
+//         int64_t result = CURRENT_STATE.REGS[d.rn] - CURRENT_STATE.REGS[d.rm];
+//         update_flags(result);
+//     } else {
+//         // SUBS - actualiza flags y guarda resultado
+//         printf("subs_reg function enter\n");
+//         NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] - CURRENT_STATE.REGS[d.rm];
+//         update_flags(NEXT_STATE.REGS[d.rd]);
+//     }
+// }
+
+void subs_reg(uint32_t instruction) {
+    DecodedInstr d = decode_instruction(instruction, 0); // 0 indica tipo R
+
+    uint32_t operand1 = CURRENT_STATE.REGS[d.rn];
+    uint32_t operand2 = CURRENT_STATE.REGS[d.rm];
+    
+    uint32_t shift_type = get_bits(instruction, 21, 22);
+    uint32_t shift_amount = get_bits(instruction, 10, 15);
+
+    switch (shift_type) {
+        case 0: // LSL (Logical Shift Left)
+            operand2 = operand2 << shift_amount;
+            break;
+        case 1: // LSR (Logical Shift Right)
+            operand2 = operand2 >> shift_amount;
+            break;
+        case 2: // ASR (Arithmetic Shift Right)
+            operand2 = (int32_t)operand2 >> shift_amount;
+            break;
+        case 3: // ROR (Rotate Right)
+            operand2 = (operand2 >> shift_amount) | (operand2 << (32 - shift_amount));
+            break;
+        default:
+            return;
+    }
+
+    int32_t result = (int32_t)operand1 - (int32_t)operand2;
+    update_flags(result);
+    
+    if (d.rd != 31) {
+        NEXT_STATE.REGS[d.rd] = result;
     }
 }
 
 void add(uint32_t instruction) {
-    printf("add function enter\n");
     DecodedInstr d = decode_instruction(instruction, 0);
     NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] + CURRENT_STATE.REGS[d.rm];
 }
 
 void mul (uint32_t instruction){
-    printf("mul function enter\n");
     DecodedInstr d = decode_instruction(instruction, 0);
     NEXT_STATE.REGS[d.rd] = CURRENT_STATE.REGS[d.rn] * CURRENT_STATE.REGS[d.rm];
 }
